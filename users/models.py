c8e1ffd20db35f4d3d8db3a9e7474b8c0f652cdb
eunhyung <ooooo5h91@gmail.com>
2022-07-15 07:40:10 +0900

    core 앱 및 추상클래스 생성

--- users/models.py
+++ users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
+from core.models import TimeStampModel
+
 # 모델의 이름은 단수
-class User(models.Model):
+class User(TimeStampModel):
     name     = models.CharField(max_length=50)
     email    = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=300)
     phone    = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     
     # 데이터베이스 내에서 테이블을 식별할 때 사용하는 이름
     class Meta:
