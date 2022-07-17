1900c1bcaa05e97de0de7d21af50b9604904dbf4
eunhyung <ooooo5h91@gmail.com>
2022-07-17 16:04:44 +0900

    로그인 JWT

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 import bcrypt
 import json
+import jwt
 
+from django.conf import settings
 from django.http  import JsonResponse
 from django.views import View
 
@@ -43,10 +45,17 @@ class SignInView(View):
             email    = data['email']
             password = data['password']
             
-            if not User.objects.filter(email=email, password=password):
-                return JsonResponse({'message':'Email or Password Invalid'}, status=401)
+            email_exist_user = User.objects.get(email=email)
             
-            return JsonResponse({'message':'SUCCESS'}, status=200)
+            if not email_exist_user :
+                return JsonResponse({'message':'Email is Invalid'}, status=401)
+            
+            if not bcrypt.checkpw(password.encode('utf-8'), email_exist_user.password.encode('utf-8')):
+                return JsonResponse({'message':'Password is Invalid'}, status=401)
+            
+            token = jwt.encode({'user_id':email_exist_user.id}, settings.SECRET_KEY, settings.ALGORITHM)
+            
+            return JsonResponse({'message':'SUCCESS', 'token':token}, status=200)
             
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
