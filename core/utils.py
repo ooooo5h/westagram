95525778ab8dcd96a364fd861513a38e0b411996
eunhyung <ooooo5h91@gmail.com>
2022-07-18 09:15:18 +0900

    로그인 데코레이터 수정

--- core/utils.py
+++ core/utils.py
@@ -8,16 +8,22 @@ from users.models import User
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try :
-            token        = request.headers.get('Authorization', None)
+            if 'Authorization' not in request.headers:
+                # Authorization이 없이 전달된 경우
+                return JsonResponse({'message':'KEY_ERROR'}, status=400)
+            
+            token        = request.headers.get('Authorization')
             payload      = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
-            user         = User.objects.get(id=payload['user_id'])
+            user         = User.objects.get(id=payload['user_id'])  # 이 키를 이용해서?
             request.user = user            
             
         except jwt.exceptions.DecodeError:
+            # 우리가 보낸 토큰이 아닐 때 (서명)
             return JsonResponse({'message':'INVALID_TOKEN'}, status=400)
         
-        except User.DoesNotExist:                                           
+        except User.DoesNotExist:           
+            # db에 유효하지않은 유저일 때                                 
             return JsonResponse({'message' : 'INVALID_USER'}, status=400)
-        
+                
         return func(self, request, *args, **kwargs)
     return wrapper
